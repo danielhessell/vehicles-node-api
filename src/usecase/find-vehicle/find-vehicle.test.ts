29132718d413fc5b1377bd5f1f1a2a934f37f1ea
Daniel Hessel <danieldaniabreu@gmail.com>
2025-02-05 15:43:20 -0300

    test: find vehicle unit test

---
+++ src/usecase/find-vehicle/find-vehicle.test.ts
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import sinon from "sinon";
+import { Vehicle } from "src/entity/vehicle.entity";
+import { FindVehicle } from "./find-vehicle";
+
+describe("Find Vehicle", () => {
+  it("should find a vehicle by id", async () => {
+    const vehicle = Vehicle.create({
+      placa: "ABC1234",
+      chassi: "12345678901234567",
+      renavam: "12345678901",
+      ano: 2020,
+      modelo: "Uno",
+      marca: "Fiat",
+    });
+
+    const repository = {
+      findByDocument: sinon.spy(),
+      add: sinon.spy(),
+      findById: sinon.spy(() => Promise.resolve(vehicle)),
+    };
+
+    const usecase = new FindVehicle(repository);
+
+    await usecase.execute({ id: vehicle.id });
+
+    expect(repository.findById.calledOnce).to.be.true;
+  });
+
+  it("should throw an error when vehicle does not exist", async () => {
+    const repository = {
+      findByDocument: sinon.spy(),
+      add: sinon.spy(),
+      findById: sinon.spy(() => Promise.resolve(null)),
+    };
+
+    const usecase = new FindVehicle(repository);
+
+    try {
+      await usecase.execute({ id: "123" });
+    } catch (error) {
+      expect(error.message).to.equal("Veículo não encontrado.");
+    }
+  });
+});
