a7a1d78bc73110cc325e0fa43e96854239ce1714
Daniel Hessel <danieldaniabreu@gmail.com>
2025-02-05 16:47:54 -0300

    chore: add gracefull shutdown strategy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,49 @@ import "reflect-metadata";
 import "./container";
 import { Server } from "./server";
 
-(async () => {
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(
+    `App exiting due to an unhandled promise: ${promise} and reason: ${JSON.stringify(
+      reason
+    )}`
+  );
+  throw reason;
+});
+
+process.on("uncaughtException", (error) => {
+  console.log(
+    `App exiting due to an uncaught exception: ${JSON.stringify(error)}`
+  );
+  process.exit(ExitStatus.Failure);
+});
+
+process.removeAllListeners("warning");
+
+(async (): Promise<void> => {
   try {
     const server = new Server();
     await server.start();
+
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+    for (const exitSignal of exitSignals) {
+      process.on(exitSignal, async () => {
+        try {
+          await server.stop();
+          console.log("App exited with success");
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          console.log(`App exited with error: ${JSON.stringify(error)}`);
+          process.exit(ExitStatus.Failure);
+        }
+      });
+    }
   } catch (error) {
     console.log(`App exited with error: ${JSON.stringify(error)}`);
+    process.exit(ExitStatus.Failure);
   }
 })();
